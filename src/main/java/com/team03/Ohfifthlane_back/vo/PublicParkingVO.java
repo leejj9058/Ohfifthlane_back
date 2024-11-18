8233d3012238783b0b574e702d5d912c9459b08d
yujin-kim <qkqh5682@naver.com>
2024-11-18 15:13:37 +0900

    공영주차장 마커완

---
+++ src/main/java/com/team03/Ohfifthlane_back/vo/PublicParkingVO.java
@@ -0,0 +1,51 @@
+package com.team03.Ohfifthlane_back.vo;
+
+import java.math.BigDecimal;
+
+public class PublicParkingVO {
+
+	private int publicParkingId;
+	private String publicParkingName;
+	private String publicParkingAddress;
+	private BigDecimal publicParkingLat;
+	private BigDecimal publicParkingLon;
+	public int getPublicParkingId() {
+		return publicParkingId;
+	}
+	public void setPublicParkingId(int publicParkingId) {
+		this.publicParkingId = publicParkingId;
+	}
+	public String getPublicParkingName() {
+		return publicParkingName;
+	}
+	public void setPublicParkingName(String publicParkingName) {
+		this.publicParkingName = publicParkingName;
+	}
+	public String getPublicParkingAddress() {
+		return publicParkingAddress;
+	}
+	public void setPublicParkingAddress(String publicParkingAddress) {
+		this.publicParkingAddress = publicParkingAddress;
+	}
+	public BigDecimal getPublicParkingLat() {
+		return publicParkingLat;
+	}
+	public void setPublicParkingLat(BigDecimal publicParkingLat) {
+		this.publicParkingLat = publicParkingLat;
+	}
+	public BigDecimal getPublicParkingLon() {
+		return publicParkingLon;
+	}
+	public void setPublicParkingLon(BigDecimal publicParkingLon) {
+		this.publicParkingLon = publicParkingLon;
+	}
+	@Override
+	public String toString() {
+		return "PublicParkingVO [publicParkingId=" + publicParkingId + ", publicParkingName=" + publicParkingName
+				+ ", publicParkingAddress=" + publicParkingAddress + ", publicParkingLat=" + publicParkingLat
+				+ ", publicParkingLon=" + publicParkingLon + "]";
+	}
+	
+	
+	
+}
