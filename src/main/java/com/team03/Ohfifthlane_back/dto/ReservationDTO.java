a389a3bc41137e4fc410e3a48a71d4e14f8c1154
dbrnjstn <lkk030400@gmail.com>
2024-11-19 16:00:36 +0900

    예약 디비 추가 가능 및 DTO 추가
    예약 디비 추가 가능 및 DTO 추가

---
+++ src/main/java/com/team03/Ohfifthlane_back/dto/ReservationDTO.java
@@ -0,0 +1,61 @@
+package com.team03.Ohfifthlane_back.dto;
+
+import java.sql.Timestamp;
+import java.time.LocalTime;
+
+public class ReservationDTO {
+	
+	private int userId;	
+	private LocalTime reservationStartTime;
+	private LocalTime reservationEndTime;
+	private String reservationDay;
+	private int reservationTotalFee;
+	private int rpzId;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public LocalTime getReservationStartTime() {
+		return reservationStartTime;
+	}
+	public void setReservationStartTime(LocalTime reservationStartTime) {
+		this.reservationStartTime = reservationStartTime;
+	}
+	public LocalTime getReservationEndTime() {
+		return reservationEndTime;
+	}
+	public void setReservationEndTime(LocalTime reservationEndTime) {
+		this.reservationEndTime = reservationEndTime;
+	}
+	public String getReservationDay() {
+		return reservationDay;
+	}
+	public void setReservationDay(String reservationDay) {
+		this.reservationDay = reservationDay;
+	}
+	public int getReservationTotalFee() {
+		return reservationTotalFee;
+	}
+	public void setReservationTotalFee(int reservationTotalFee) {
+		this.reservationTotalFee = reservationTotalFee;
+	}
+	public int getRpzId() {
+		return rpzId;
+	}
+	public void setRpzId(int rpzId) {
+		this.rpzId = rpzId;
+	}
+	@Override
+	public String toString() {
+		return "ReservationDTO [userId=" + userId + ", reservationStartTime=" + reservationStartTime
+				+ ", reservationEndTime=" + reservationEndTime + ", reservationDay=" + reservationDay
+				+ ", reservationTotalFee=" + reservationTotalFee + ", rpzId=" + rpzId + "]";
+	}
+
+	
+	
+	
+}
